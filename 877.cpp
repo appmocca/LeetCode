91b9a44c36af41696c6ae372cb303375e08c9642
Yifei Xu <appmocca@gmail.com>
2021-06-16 16:34:05 +0800

    update daily question

---
+++ 877.cpp
@@ -0,0 +1,11 @@
+// Since in this case, alex will always win
+// If he pick first, the sum of rocks is odd & piles are even.
+// No matter how Lee choose, Alex will always has the most rocks.
+// So only need to return true, because Alex never lose.
+
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true ;
+    }
+};
