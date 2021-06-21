4db968abf0033d9550dd0b52b2206553d9e773db
Yifei Xu <appmocca@gmail.com>
2021-06-21 19:52:10 +0800

    update

---
+++ 剑指 Offer 24.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+#define LN ListNode
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        LN *pre = nullptr, *curr = head ;
+        while (curr){
+            LN *tmp = curr -> next ;
+            curr -> next = pre ;
+            pre = curr ;
+            curr = tmp ;
+        }
+        return pre ;
+    }
+};
