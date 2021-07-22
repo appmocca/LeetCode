074a0b1024ddb6bc3e385ceb53a408b11a463a0c
Yifei Xu <appmocca@gmail.com>
2021-07-22 22:51:54 +0800

    update

---
+++ M138.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    unordered_map<Node*, Node*> cachedNode;
+
+    Node* copyRandomList(Node* head) {
+        if (!head) return nullptr;
+        if (!cachedNode.count(head)) {
+            Node* headNew = new Node(head->val);
+            cachedNode[head] = headNew;
+            headNew->next = copyRandomList(head->next);
+            headNew->random = copyRandomList(head->random);
+        }
+        return cachedNode[head];
+    }
+};
